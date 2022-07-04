5828e2d9e14097e77dd9e59fc264e27e1586d711
NikitaShevchik <shevchik.nikita@gmail.com>
2022-07-04 15:23:53 +0300

    done final and draw

--- js/scrypt.js
+++ js/scrypt.js
@@ -2,25 +2,114 @@
 
 const tictacField = document.querySelectorAll('.tictac__cell');
 const tictacBoard = document.querySelector('.main__tictac');
+const resultShow = document.querySelector('.main__result');
+const winnerField = document.querySelector('.main__winner');
+
+
+const winCombinations = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+]
 
 {/* <div class="cross"></div>
 <div class="circle"></div>
 <div class="cross"></div>
 <div class="circle"></div> */}
 
-for (let c of tictacField) {
-    c.addEventListener('click', () => clickTic(c))
-}
-var i = 0;
-function clickTic(cell) {
-    if (cell.innerHTML == '') {
-        cell.innerHTML = ['<div class="cross"></div>', '<div class="circle"></div>'][i % 2]
-        i++;
+// for (let c of tictacField) {
+//     c.addEventListener('click', () => clickTic(c))
+// }
+// var i = 0;
+// function clickTic(cell) {
+//     if (cell.innerHTML == '') {
+//         cell.innerHTML = ['<div class="cross"></div>', '<div class="circle"></div>'][i % 2]
+//         i++;
+//     }
+//     // ifWinLite(tictacField)
+//     ifWinX(tictacField)
+// }
+
+// var win = false;
+
+
+
+function tictacGame() {
+    var i = 0; // счетчик ходов
+    var win = false;
+    var draw = false;
+    for (let c of tictacField) {
+        c.addEventListener('click', function (k) {
+            if (!win) {
+                if (c.innerHTML == '') {
+                    c.innerHTML = ['<div class="cross"></div>', '<div class="circle"></div>'][i % 2]
+                    i++;
+                }
+                ifWinX(tictacField)
+            } else {
+                k.preventDefault()
+            }
+            ifDraw()
+            if (draw) {
+                k.preventDefault()
+                tictacBoard.classList.add('_win');
+                resultShow.classList.add('_show');
+                winnerField.innerHTML = `Победила дружба! (ничья)`;
+            }
+        })
     }
-    // ifWinLite(tictacField)
-    ifWinX(tictacField)
+    function ifWinX(game) {
+        for (let q = 0; q < game.length; q++) {
+            if (!win) {
+                if (game[q].innerHTML != '') {
+                    for (let w of winCombinations) {
+                        if (game[q].innerHTML == game[w[0]].innerHTML &&
+                            game[q].innerHTML == game[w[1]].innerHTML &&
+                            game[q].innerHTML == game[w[2]].innerHTML &&
+                            game[q].innerHTML != '') {
+                            win = true;
+                            winGame();
+                        }
+                    }
+                }
+            }
+        }
+    }
+    function winGame() {
+        tictacBoard.classList.add('_win');
+        if (i % 2 == 0) {
+            resultShow.classList.add('_show');
+            winnerField.innerHTML = `Победили кружки!`;
+        } else {
+            resultShow.classList.add('_show');
+            winnerField.innerHTML = `Победили крестики!`;
+        }
+    }
+    function ifDraw() {
+        for (let c of tictacField) {
+            if (c.innerHTML != '' && i == 9 && !win) {
+                draw = true;
+            }
+        }
+    }
 }
 
+tictacGame()
+
+
+
+
+
+
+
+
+
+/*------------------------*/
 
 function ifWin(f) {
     console.log('+')
@@ -101,7 +190,6 @@ function ifWin(f) {
         }
     }
 }
-
 function ifWinLite(f) {
     console.log('+')
     if (f[0].innerHTML != '') {
@@ -136,40 +224,3 @@ function ifWinLite(f) {
         }
     }
 }
-// || f[3].innerHTML == f[4].innerHTML == f[5].innerHTML || f[6].innerHTML == f[7].innerHTML == f[8].innerHTML
-
-const winCombinations = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8]
-]
-var win = false;
-function ifWinX(game) {
-    for (let q = 0; q < game.length; q++) {
-        if (!win) {
-            if (game[q].innerHTML != '') {
-                for (let w of winCombinations) {
-                    if (game[q].innerHTML == game[w[0]].innerHTML &&
-                        game[q].innerHTML == game[w[1]].innerHTML &&
-                        game[q].innerHTML == game[w[2]].innerHTML &&
-                        game[q].innerHTML != '') {
-                        win = true;
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-function win() {
-    console.log('Победа');
-    tictacBoard.classList.add('_win')
-}
-
-
